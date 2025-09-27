f1952637dfbfd637d4ef0f6ed84f94c2572692a9
itsbrd <142621528+itsbrd@users.noreply.github.com>
2025-09-27 18:32:11 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,12 @@ document.addEventListener('DOMContentLoaded', () => {
   const ageGate = document.getElementById('age-gate');
   const enterBtn = document.getElementById('enter-btn');
   const mainContent = document.getElementById('main-content');
-
+  
+  const supabase = supabase.createClient(
+    'https://qmlhagpdfnckuufhhixu.supabase.co',  // replace with your actual URL
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFtbGhhZ3BkZm5ja3V1ZmhoaXh1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkwMTEzMTIsImV4cCI6MjA3NDU4NzMxMn0.HFrgVdnETfPL6EDa9lrj0SwZkEFehMbDUjvkq7VkRTk'                  // replace with your anon public key
+  );
+  
   const showAgeGate = () => {
     ageGate.style.display = 'flex';
     mainContent.style.display = 'none';
