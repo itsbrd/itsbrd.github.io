33f840f8f4684043939c8fab07fd2d9d4dc30b9a
itsbrd <142621528+itsbrd@users.noreply.github.com>
2026-03-04 19:08:43 -0500

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -2,6 +2,6 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
 export const supabase = createClient(
-  "https://YOUR_PROJECT_REF.supabase.co",
-  "YOUR_SUPABASE_ANON_OR_PUBLISHABLE_KEY"
+  "https://fauoohclvblciogeluiy.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZhdW9vaGNsdmJsY2lvZ2VsdWl5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE2MTAzNzgsImV4cCI6MjA4NzE4NjM3OH0.uZlfIOpsf6ezDPfh9LhONsY2DsDQ9RQf2IRec1WXYWE"
 );
